04f384d3c5ff94191356f0f59faadbf4d384047f
Oscar Beaumont <oscar@otbeaumont.me>
2024-07-01 20:50:49 +0800

    Add `@tailwindcss/typography`

--- src/app/docs.tsx
+++ src/app/docs.tsx
@@ -23,7 +23,7 @@ function Navigation() {
 										href={`/docs/${link.href}`}
 										class={clsx(
 											"block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full",
-											link.href === location.pathname
+											`/docs/${link.href}` === location.pathname
 												? "font-semibold text-sky-500 before:bg-sky-500"
 												: "text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300",
 										)}
@@ -49,7 +49,9 @@ export default function Page(props: ParentProps) {
 				</div>
 			</div>
 
-			<div class="p-4">{props.children}</div>
+			<div class="p-4 prose prose-invert lg:prose-md prose-white">
+				{props.children}
+			</div>
 		</div>
 	);
 }
