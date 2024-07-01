706dc6639053231d709888f6dd7b275f19d32747
Oscar Beaumont <oscar@otbeaumont.me>
2024-07-01 12:50:03 +0800

    Add Biome formatter + format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+	integrations: [tailwind(), solidJs()],
 });
