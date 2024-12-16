6e422a40f9ebc4d63ee83c9970fda7003cd4bca0
Oscar Beaumont <oscar@otbeaumont.me>
2024-12-16 23:24:19 +0800

    use edge runtime

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,6 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
+
+export const runtime = "edge";
 
 export const { GET } = createFromSource(source);
