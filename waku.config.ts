c5da7a5ec1675da197b70198126479f049eb3c1b
Oscar Beaumont <oscar@otbeaumont.me>
2026-01-13 10:52:00 +0800

    fix fonts

--- waku.config.ts
+++ waku.config.ts
@@ -2,13 +2,22 @@ import { type Config, defineConfig } from "waku/config";
 import mdx from "fumadocs-mdx/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { DEFAULT_CATEGORY_FALLBACKS, FontaineTransform } from "fontaine";
 import type { UserConfig } from "vite";
 import * as MdxConfig from "./source.config.js";
 import { headersPlugin } from "./src/vite-plugin-headers";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), mdx(MdxConfig), tsconfigPaths(), headersPlugin()],
+    plugins: [
+      tailwindcss(),
+      mdx(MdxConfig),
+      tsconfigPaths(),
+      headersPlugin(),
+      FontaineTransform.vite({
+        fallbacks: DEFAULT_CATEGORY_FALLBACKS,
+      }),
+    ],
     optimizeDeps: {
       // https://github.com/fuma-nama/fumadocs/issues/2845#issuecomment-3707856896
       exclude: ["fumadocs-ui", "fumadocs-core", "@fumadocs/ui"],
