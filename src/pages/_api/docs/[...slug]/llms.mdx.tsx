bea7f09e914dfa92f8714fe4a8e0dea7151c13e2
Oscar Beaumont <oscar@otbeaumont.me>
2026-07-01 11:30:12 +0800

    Cleanup Waku Routing

--- src/pages/_api/docs/[...slug]/llms.mdx.tsx
+++ src/pages/_api/docs/[...slug]/llms.mdx.tsx
@@ -1,10 +1,11 @@
-import { getLLMText, getNonRootDocStaticPaths, source } from "@/lib/source";
+import { getLLMText, source } from "@/lib/source";
+import type { ApiContext } from "waku/router";
 
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const path = removeSuffix(url.pathname, "/llms.mdx");
-  const slug = path === "/docs" ? [] : removePrefix(path, "/docs/").split("/");
-
+export async function GET(
+  _request: Request,
+  { params }: ApiContext<"/_api/docs/[...slug]/llms.mdx">,
+) {
+  const slug = params.slug;
   const page = source.getPage(slug);
   if (!page)
     return new Response("# 404 Not Found", {
@@ -20,14 +21,12 @@ export async function GET(request: Request) {
 }
 
 export async function getConfig() {
+  const pages = source
+    .generateParams()
+    .map((item) => (item.lang ? [item.lang, ...item.slug] : item.slug));
+
   return {
     render: "static",
-    staticPaths: getNonRootDocStaticPaths(),
+    staticPaths: pages,
   } as const;
 }
-
-const removePrefix = (s: string, p: string) =>
-  s.startsWith(p) ? s.slice(p.length) : s;
-
-const removeSuffix = (s: string, p: string) =>
-  s.endsWith(p) ? s.slice(0, -p.length) : s;
